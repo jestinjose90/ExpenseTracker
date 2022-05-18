d631d6aec118d4d73b424c32f984cf46862f03a6
Jestin Jose <fjestinjose@neiu.edu>
2022-05-17 19:12:34 -0500

    updated

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 function Tracker() {
-    return <h1>Expense Tracker</h1>
+    const[amount, setAmount] = useState(0)
+    const expense = amount - 10;
+    return (<div className="create">
+<h1>Expense Tracker</h1>
+<form>
+    <label>Enter the amount</label>
+    <input type="number" required value={amount}>
+    </input>
+
+ 
+</form>
+    </div>
+    )
 }
 
 export default Tracker;
